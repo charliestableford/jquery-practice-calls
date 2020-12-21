4c07d835076c2fa270e58db005e9d1e3c4c56372
Charlie Stableford <charliestableford2@gmail.com>
2020-12-21 15:58:49 -0500

    functional not styled

--- script.js
+++ script.js
@@ -2,33 +2,72 @@
 
 var key = "406fb5b1";
 	
-	$(".results").on("click"); 
-
-	$("form").on("submit", function(e) {
-		e.preventDefault();
-        var searchInput = $("input").val()
-        ,i = 0
-        ,movieTitle;
-        
-		$.ajax({
-			url: "http://www.omdbapi.com/",
-			method: "get",
-			data: {
-                "s": searchInput, 
-                "callback": "movies",
-                "apikey": `${key}`
-            },
-			dataType: "jsonp",
-			jsonpCallback: "movies",
-			success: function(data) {
-				console.log(data);
-				var movies = data.Search;
-				$(".results").empty();
-				for (i = 0; i < movies.length; i++) {
-					console.log(movies[i].Title);
-					movieTitle = movieLi(movies[i]);
-					$(".results").append(movieTitle);
-				}
-			}
-		});
-	});
+function movieLi(obj){
+    return $('<li class="result" data-id="' + obj.imdbID +
+  '"><strong>' + obj.Title + '</strong><em> (' + obj.Year + ')</em></li>');
+  }
+  
+  function moviePoster(url){
+      return $('<img src="' +url+ '" alt="Movie Poster" />');
+  }
+  
+  function getMovieDetails(id){
+      // alert(id);
+      $.ajax({
+              url: "http://www.omdbapi.com/",
+              method: "get",
+              data: {
+                  "i": id, 
+                  "callback": "movies", 
+                  "apikey": `${key}`
+                },
+              dataType: "jsonp",
+              jsonpCallback: "movies",
+              success: function(data) {
+                  console.log(data);
+                  var posterUrl = data.Poster;
+                  var moviePosterImg = moviePoster(posterUrl);
+                  $('.movieDets').empty().append(moviePosterImg);
+              }
+          });
+  }
+  
+  function getID(){
+      var id = $(this).attr("data-id");
+      getMovieDetails(id);
+  }
+  
+  // jQuery Main Function
+  $(function() {
+      
+      $(".results").on("click", "li", getID); 
+  
+      $("form").on("submit", function(e) {
+          e.preventDefault();
+          var searchTerm = $("input").val()
+              , i = 0
+              , movieTitle
+          ;
+          // console.log(searchTerm);
+          $.ajax({
+              url: "http://www.omdbapi.com/",
+              method: "get",
+              data: {
+                  "s": searchTerm, 
+                  "callback": "movies",
+                  "apikey": `${key}`
+                },
+              dataType: "jsonp",
+              jsonpCallback: "movies",
+              success: function(data) {
+                //   console.log(data);
+                  var movies = data.Search;
+                  $(".results").empty();
+                  for (i = 0; i < movies.length; i++) {
+                      movieTitle = movieLi(movies[i]);
+                      $(".results").append(movieTitle);
+                  }
+              }
+          });
+      });
+  });
